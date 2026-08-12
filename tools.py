a953c1c1c026a62911a46c853ae0be2012a82cd0
mourya505 <127108201+mourya505@users.noreply.github.com>
2026-08-12 14:38:16 +0000

    commit

--- tools.py
+++ tools.py
@@ -65,16 +65,16 @@ def unique_column(column_name:str):
     return df[column_name].unique().tolist()
 @tool
 def sales_growth(start:str,end:str):
-    """to find sales of two quarters
+    """use this when ever claculating growth percentage
     Q1 or q1 must be Q1_sales
     Q2 or q2 must be Q2_sales
     Q3 or q3 must be Q3_sales
     Q4 or q4 must be Q4_sales
     """
-    start_sales=total_sales(start)
+    start_sales=total_sales.invoke(start)
     if start_sales==0:
         return
-    end_sales=total_sales(end)
+    end_sales=total_sales.invoke(end)
     result=((end_sales-start_sales)/start_sales)*100
     return result
 @tool
